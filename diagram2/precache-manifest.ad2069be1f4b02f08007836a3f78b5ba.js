1230a3f130effa2e4d1b14ba9b9ae9c049b260e2
Nevo David <nevod@codeoasis.com>
2020-07-18 17:39:20 +0700

    feat(): digram2

---
+++ diagram2/precache-manifest.ad2069be1f4b02f08007836a3f78b5ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1e232e98c2750399c7bf6493f3f7458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "925d412f536058f10607",
+    "url": "/static/css/main.ff839566.chunk.css"
+  },
+  {
+    "revision": "0315ef1784c303f3a846",
+    "url": "/static/js/2.3c945de0.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/static/js/2.3c945de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925d412f536058f10607",
+    "url": "/static/js/main.e73b71b3.chunk.js"
+  },
+  {
+    "revision": "8acef4bd45c4bd308cf1",
+    "url": "/static/js/runtime-main.160ced06.js"
+  }
+]);
